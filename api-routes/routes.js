fc3eb515509fa983dcf55ebb8e7817c160c7907d
Neerajraja10 <raja.n@northeastern.edu>
2023-02-09 06:42:32 -0500

    f2

--- api-routes/routes.js
+++ api-routes/routes.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var user = require('../services/user');
-var helper = require('../config/config');
+var helper = require('../config/helper');
 router.post('/', user.newUser);
-router.get('/:id', helper.bAuthCheck, user.getUser);
-router.put('/:id', helper.bAuthCheck, user.updateUser);
+router.get('/:id', helper.uAuthCheck, user.getUser);
+router.put('/:id', helper.uAuthCheck, user.updateUser);
 module.exports = router;
